8899e20dac9a92cf5e03d34ec95ebdad6322b846
alexanderpierce <takaaki.110706@gmail.com>
2023-08-09 00:15:27 +0900

    Add plain counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Counter } from "./Counter"
+
 export const App = () => {
   return (
     <div>
       <p>Hello Vite</p>
+      <Counter />
     </div>
   )
 }
---
+++ src/Counter.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {counter}</h1>
+      <button onClick={() => {setCounter((prev) => prev + 1)}}>+</button>
+      <button onClick={() => {setCounter((prev) => prev - 1)}}>-</button>
+    </div>
+  )
+}
